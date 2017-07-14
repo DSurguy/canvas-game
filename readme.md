b8085e710ca41fb4e8ea9cc3d0e461ba18404160
Derek Surguy <d.surguy@austinlane.com>
2017-07-14 16:57:07 -0500

    Initial commit with project setup

---
+++ readme.md
@@ -0,0 +1,5 @@
+## Initial Goals
+1. Create room with wall collision
+2. Create player with wasd movement
+3. Add mouse click movement to player
+4. Add mouse hold movement to player
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+module.exports = {
+    entry: ["babel-polyfill", './app.js'],
+    output: {
+        publicPath: '/assets/',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env'],
+                        cacheDirectory: './node_modules/.cache/babel-loader'
+                    }
+                }
+            }
+        ]
+    }
+}
